cd9adf480cf646ca5eccf9143c1067bf2bc04c77
sachin kanadia <sachinkanadia@hotmail.com>
2019-05-31 13:05:41 +0100

    Configured screen orientation

---
+++ News App/News App/News App.Android/LaunchActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace News_App.Droid
+{
+    [Activity(
+             Label = "News App",
+             Theme = "@style/Theme.Splash", //Indicates the theme to use for this activity
+             MainLauncher = true, //Set it as boot activity
+             NoHistory = true)] //Doesn't place it in back stack
+    public class LaunchActivity : Activity
+    {
+        //Need to move to main activity.
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
--- News App/News App/News App.Android/MainActivity.cs
+++ News App/News App/News App.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Lottie.Forms.Droid;
 
 namespace News_App.Droid
 {
-    [Activity(Label = "News_App", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "News_App", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
